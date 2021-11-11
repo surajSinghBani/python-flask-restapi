c5981d05ef8720bfbfee998bcf5297eab4b242d4
surajSinghBani <suraj.singh@algoworks.com>
2021-11-11 14:49:49 +0530

    added err file

---
+++ err.py
@@ -0,0 +1,50 @@
+i=0
+while i<=10:
+    print("I will run forever!!!")
+
+i=0
+while i<=10:
+    print("I will run forever!!!")
+    
+
+i=0
+while i<=10:
+    print("I will run forever!!!")
+
+i=0
+while i<=10:
+    print("I will run forever!!!")
+
+i=0
+while i<=10:
+    print("I will run forever!!!")
+
+i=0
+while i<=10:
+    print("I will run forever!!!")
+
+i=0
+while i<=10:
+    print("I will run forever!!!")
+    
+
+i=0
+while i<=10:
+    print("I will run forever!!!")
+
+i=0
+while i<=10:
+    print("I will run forever!!!")
+
+i=0
+while i<=10:
+    print("I will run forever!!!")
+
+
+i=0
+while i<=10:
+    print("I will run forever!!!")
+
+i=0
+while i<=10:
+    print("I will run forever!!!")
